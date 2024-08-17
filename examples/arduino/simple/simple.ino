598c0720ff9064210a2027e8a3c8c9932b1b2ef8
Johan Böhlin <johboh@gmail.com>
2024-08-17 10:15:15 +0200

    Remove incompatible Arudino IDE support

--- examples/arduino/simple/simple.ino
+++ examples/arduino/simple/simple.ino
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <nlohmann-json.hpp>
+#include <nlohmann/json.hpp>
 
 void setup() {
   nlohmann::json doc;
--- src/nlohmann-json.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __NLOHMANN_JSON_H__
-#define __NLOHMANN_JSON_H__
-
-#include <nlohmann/json.hpp>
-#include <nlohmann/json_fwd.hpp>
-
-#endif  //__NLOHMANN_JSON_H__
